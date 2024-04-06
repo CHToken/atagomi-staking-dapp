d873730e821770819065834a1d06e7a3fef0f18c
CHToken <chtokenx@gmail.com>
2024-04-06 14:10:29 +0100

    shit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Route path="/" element={<StakingPage />} />
       </Routes>
       <Footer />
-    </div> 
+    </div>
   );
 }
 
--- src/pages/StakingPage/StakingPage.js
+++ src/pages/StakingPage/StakingPage.js
@@ -1,10 +1,10 @@
-// import s from "./StakingPage.module.css";
+import s from "./StakingPage.module.css";
 import NewInfo from "../../components/NewInfo/NewInfo";
 import Cards from "../../components/Cards/Cards";
 
 function StakingPage() {
   return (
-    <div>
+    <div className={s.root}>
       <NewInfo />
       <Cards />
     </div>
